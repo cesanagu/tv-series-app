a0f166dcc6d5324db194011c5cec674b3fbaa388
Cesar Aguilar <cesar_aguilar.parraga@hotmail.com>
2021-11-28 20:56:58 -0500

    gh-pages

--- src/containers/SingleSeries/index.js
+++ src/containers/SingleSeries/index.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, useParams } from "react-router-dom";
 import Loader from "../../components/loader";
 
+function idParams() {
+  let { Id } = useParams();
+  console.log(Id);
+  return <div>your expected id : {Id}</div>;
+}
+
 class SingleSeries extends Component {
   state = {
     show: null,
@@ -10,23 +16,17 @@ class SingleSeries extends Component {
   //Aquí quiero utilizar el valor del RESULTADO DE IdParams que seria el id, para poderlo pasar en el link del fetch
 
   componentDidMount() {
-    const IdParams = () => {
-      let { id } = useParams();
-      return { id };
-    };
-    console.log(IdParams);
-
-    fetch(`http://api.tvmaze.com/shows/${IdParams}?embed=episodes`)
-      .then((response) => response.json())
-      .then((json) => this.setState({ show: json }));
+    // fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
+    //   .then((response) => response.json())
+    //   .then((json) => this.setState({ show: json }));
   }
 
   render() {
     const { show } = this.state;
-    console.log(show);
 
     return (
       <div>
+        <idParams />
         {show === null && <Loader />}
         {show !== null && (
           <div>
